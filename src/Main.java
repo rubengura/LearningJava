3378de2795dbfc626d9ddeca9370f1774d9e3604
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-13 16:38:25 +0200

    Exercise MegaBytesConverter completed.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Hello {
+public class Main {
     public static void main(String[] args){
         SpeedConverter.printConversion(2.0);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
     }
 }
