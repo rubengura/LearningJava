d3808707deaf02a38a9fc1db859616b2865f8a20
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-13 16:33:19 +0200

    Exercise SpeedConverter completed.

--- src/Hello.java
+++ src/Hello.java
@@ -1,45 +1,5 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-
-        int x = 3;
-        System.out.println(x);
-        int y = x * 2;
-        System.out.println(y);
-        System.out.println(x);
-
-        boolean condition = false;
-        if (condition)
-            System.out.println("Condition is true");
-            System.out.println("I do not belong to the if statement");
-        if (!condition){
-            System.out.println("Condition is true");
-            System.out.println("I'm inside the if statement yet");
-        }
-
-        // Ternary operations
-        int clientAge = 20;
-        //                       condition   yes return | no return
-        boolean isOverEighteen = clientAge >= 18 ? true : false;
-
-        if (isOverEighteen)
-            System.out.println("The client has more than 18 years");
-
-        boolean gameOver = true;
-        int score = 1000;
-        int level = 5;
-        int bonus = 100;
-
-        calculateScore(gameOver, score, level, bonus);
-
-    }
-
-    public static int calculateScore(boolean gameOver, int score, int level, int bonus){
-        if (gameOver){
-            int finalScore = score + (level * bonus);
-            System.out.println("Final score: " + finalScore);
-            return finalScore;
-        }
-        return -1;
+        SpeedConverter.printConversion(2.0);
     }
 }
---
+++ src/SpeedConverter.java
@@ -0,0 +1,17 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour >= 0) {
+            return Math.round(kilometersPerHour / 1.609);
+        } else {
+            return -1;
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
