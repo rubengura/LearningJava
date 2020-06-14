1a4e2006ba5102052ab32f991ef46583e10c01d5
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-14 23:43:15 +0200

    Exercise BarkingDog completed.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args){
         SpeedConverter.printConversion(2.0);
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
     }
 }
