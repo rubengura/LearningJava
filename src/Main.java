4b17782709b1cbdc5dc56a0af1750f654ef234c3
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-06-14 23:55:25 +0200

    Exercise LeapYear completed.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         SpeedConverter.printConversion(2.0);
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
         System.out.println(BarkingDog.shouldWakeUp(true, 8));
+        System.out.println(LeapYear.isLeapYear(1924));
     }
 }
