792d76b4d4a741a84100ef2d4b84cac6cb64047f
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-08-24 20:32:50 +0200

    SumOddRange exercise completed.

---
+++ src/ControlFlowStatements/SumOddRange.java
@@ -0,0 +1,22 @@
+package ControlFlowStatements;
+
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number < 0) {
+            return false;
+        } else return number % 2 == 1;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if (start < 0 || start > end) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
